4d5ba94c6fb2ef4db1063557b39ecc3de6118e93
raghu223344 <raghu0647@gmail.com>
2023-04-07 12:13:41 +0530

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+# # as 
+# and
+# assert
+# break
+# class
+# continue
+# def
+# del
+# elif
+# else
+# except
+# exec
+# nonlocal
+# for 
+# finally
+# from
+# global
+# if
+# import
+# is
+# in
+# lambda
+# not
+# or
+# pass
+# print
+# raise 
+# return
+# try
+# with
+# while
+# yield
+
+# this the 32 variyable names
+
+
+
+# (function_calls):
+ 
---
+++ practice.py
@@ -0,0 +1,14 @@
+# r= int(input("enter the radius of sphere"))
+# volume= 4/(3)*3.14*r*r*r*1
+# print(volume)
+
+
+# end def
+x = int(input("enter the x value:- "))
+y = int(input("enter the y value:- "))
+
+if x is 2*y :
+        print("the numbers are equal")
+        print(int(x))
+else:
+        print('the numbers are not equal')
